5470fcbde3b02a805f506b9dae5d1f92d6a45871
Inobody <70932717+Ekokurniawan96@users.noreply.github.com>
2020-12-01 02:30:49 +0700

    Revert "Revert "Revert "Initial commit"""
    This reverts commit 3fec76877ab247132e8be9dd47025d1a2b85d911.

--- resources/views/admin/section/footer.blade.php
+++
@@ -1,4 +0,0 @@
-    <footer class="footer text-center text-muted">
-                All Rights Reserved by Adminmart. Designed and Developed by <a
-                    href="https://wrappixel.com">WrapPixel</a>.
-     </footer>
